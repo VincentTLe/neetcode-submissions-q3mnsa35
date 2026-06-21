f1092436c50ef7751d32a49575648ca7a5274eb2
Vincent Le <154858320+VincentTLe@users.noreply.github.com>
2026-06-20 19:07:35 -0500

    Add: validate-parentheses - submission-23

---
+++ Data Structures & Algorithms/validate-parentheses/submission-23.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # String consisting bracket:  (), [], {} 
+        #  Open bracket is close by the same type of bracket
+        # edge case : odd number length of string 
+        valid = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
+        }
+        stack = []
+        for char in s:
+            # If the character is opening bracket add in stack
+            if char in "({[":
+                stack.append(char)
+            else: # If closing character then compare it to the last opening character ,  if not true return False
+                # When the stack is  empty, and opening with close bracket:
+                if not stack:
+                    return False
+
+                if valid[char] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+                    
+        return len(stack) == 0
+        
+        
